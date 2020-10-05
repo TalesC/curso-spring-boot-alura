b052d998b766695206ca68dd1cf12a2862f30dfc
TalesC <talescarvalhopereira@gmail.com>
2020-10-05 12:09:52 -0300

    adicionado jpa e database

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,7 +3,11 @@ package br.com.alura.forum.controller;
 import br.com.alura.forum.controller.dto.TopicoDTO;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +17,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
 
     @GetMapping("/")
-    public List<TopicoDTO> findAll(){
-        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-
-        return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+    public List<TopicoDTO> findAll(String nomeCurso){
+    	
+    	List<Topico> topicos = null;
+    	if(nomeCurso == null)
+    		topicos = topicoRepository.findAll();
+    	else
+    		topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+    		
+        return TopicoDTO.converter(topicos);
     }
 
 }
