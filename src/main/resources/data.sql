021be6655b8cbeec731d37a7f73f0dc3b1cbab60
TalesC <talescarvalhopereira@gmail.com>
2020-10-19 13:13:48 -0300

    add autentication

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$kAwLm9MJsCb2O7UAqSEQtupqhNa1Zo/r6of3y6K4kV8ttkZ0nvH.2');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
