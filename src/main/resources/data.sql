2ea0294c787a00617d52a6d8cddcf6d00d0fe3af
TalesC <talescarvalhopereira@gmail.com>
2021-01-26 11:06:22 -0300

    add authorization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$kAwLm9MJsCb2O7UAqSEQtupqhNa1Zo/r6of3y6K4kV8ttkZ0nvH.2');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$kAwLm9MJsCb2O7UAqSEQtupqhNa1Zo/r6of3y6K4kV8ttkZ0nvH.2');
+
+INSERT INTO PERFIL(id, nome) VALUES (1, 'ROLE_ALUNO');
+INSERT INTO PERFIL(id, nome) VALUES (2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(1,1);
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(2,2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
